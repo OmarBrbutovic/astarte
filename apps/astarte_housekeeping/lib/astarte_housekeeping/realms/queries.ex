484841fe0247c49669cc812856c43c7cbfa218d2
Omar <omar.brbutovic@secomind.com>
2025-07-02 15:14:35 +0200

    refactor(HK): Function for `create_astarte_kv_store`
    Update the `create_astarte_kv_store` function to use Ecto queries
    instead of Xandra queries.
    
    The goal of this is to remove the direct use of Xandra queries in the
    and to use Ecto queries instead.
    
    Signed-off-by: Omar <omar.brbutovic@secomind.com>

--- apps/astarte_housekeeping/lib/astarte_housekeeping/realms/queries.ex
+++ apps/astarte_housekeeping/lib/astarte_housekeeping/realms/queries.ex
@@ -988,7 +988,7 @@ defmodule Astarte.Housekeeping.Realms.Queries do
     Xandra.Cluster.run(:xandra, [timeout: 60_000], fn conn ->
       with :ok <- create_astarte_keyspace(conn),
            :ok <- create_realms_table(conn),
-           :ok <- create_astarte_kv_store(conn),
+           :ok <- create_astarte_kv_store(),
            :ok <- insert_astarte_schema_version(conn) do
         :ok
       else
@@ -1067,7 +1067,7 @@ defmodule Astarte.Housekeeping.Realms.Queries do
     end
   end
 
-  defp create_astarte_kv_store(conn) do
+  defp create_astarte_kv_store do
     query = """
     CREATE TABLE #{Realm.astarte_keyspace_name()}.kv_store (
       group varchar,
@@ -1080,8 +1080,8 @@ defmodule Astarte.Housekeeping.Realms.Queries do
 
     consistency = Consistency.domain_model(:write)
 
-    with {:ok, %Xandra.SchemaChange{}} <-
-           Xandra.execute(conn, query, %{}, consistency: consistency) do
+    with {:ok, %{rows: nil, num_rows: 1}} <-
+           Repo.query(query, [], consistency: consistency) do
       :ok
     end
   end
